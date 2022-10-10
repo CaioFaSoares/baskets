07e672eece0c8f3e26bb983c142ca926c1bcff29
Caio <caiofasoares@gmail.com>
2022-10-10 14:14:19 -0300

    Initial Commit

---
+++ syrup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  syrup
+//
+//  Created by Caio Soares on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
